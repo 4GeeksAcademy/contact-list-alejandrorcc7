f6fab54be99d4010e3506b3bb52f86cf390934ff
Alejandro <alejandrorcc24@gmail.com>
2025-02-13 20:56:20 +0000

    terminar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,47 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="col">
+			<div className="container-form col-12">
+
+			<h2>Add a new contact</h2>
+
+
+			</div>
+
+			<div className="container">
+				<div></div>
+				
+			<form action="">
+				<div>
+					<b>Full Name</b><br />
+					<input className="col-8" type="text" name="" placeholder="Full Name" />
+				</div>
+				<div>
+					<label htmlFor=""><b>Email</b></label><br />
+					<input type="text" name="" placeholder="Enter email" />
+				</div>
+				<div>
+					<label htmlFor=""><b>Phone</b></label><br />
+					<input type="text" name="" placeholder="Enter phone" />
+				</div>
+				<div>
+					<label htmlFor=""><b>Address</b></label><br />
+					<input type="text" name="" placeholder="Enter address" />
+				</div>
+
+				<div>
+					<button className="btn btn-primary">Check the Context in action</button>
+				</div>
+
+
+
+			</form>
+			</div>
 		</div>
+
 	);
 }; 
